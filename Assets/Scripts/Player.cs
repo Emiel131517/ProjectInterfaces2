ad2a791a8f770225ecce5daebe5672b8c8361358
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2021-11-02 15:11:22 +0100

    rotation fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 10;
     private int bullets = 15;
     private int score = 0;
 
@@ -20,11 +19,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
-
         Movement();
-        StrafeH(playerModel, h);
         if (Input.GetKey(KeyCode.Mouse0))
         {
             Shoot();
@@ -34,35 +29,7 @@ public class Player : MonoBehaviour
 
     public void Movement()
     {
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += transform.up * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += transform.up * -speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += transform.right * -speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += transform.right * speed * Time.deltaTime;
-        }
-    }
-
-    public void StrafeH(Transform player, float axisH)
-    {
-        Vector3 eulerAngles = player.localEulerAngles;
-        player.localEulerAngles = new Vector3(eulerAngles.x, eulerAngles.y, Mathf.LerpAngle(eulerAngles.z, axisH * 25, 0.25f));
-    }
-
-    public void RotateUpDown(Transform player, float axisV)
-    {
-        Vector3 eulerAngles = player.localEulerAngles;
-        player.localEulerAngles = new Vector3(Mathf.LerpAngle(eulerAngles.x, -axisV * 25, 0.25f), eulerAngles.y, eulerAngles.z);
+        
     }
 
     public void Shoot()
